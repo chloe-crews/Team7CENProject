e97ae8265c28bfbd0ded41fb7e6618d0a5acdf9a
Arwen Dowers <arwendowers@gmail.com>
2024-11-04 11:32:33 -0500

    started login page UI and started connecting to database

---
+++ accounts/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'email', 'username', 'password']
+
+    def create(self, validated_data):
+        # Use the CustomUser manager to create a user
+        user = CustomUser.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CustomUserSerializer
+from .models import CustomUser
 
-# Create your views here.
 def home(request):
+
     return render(request, 'accounts/dashboard.html')
     # return HttpResponse('<h1>Welcome to Wear & Share!</h1>')
+
+class CreateUserView(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteUserView(APIView):
+    def delete(self, request, user_id):
+        user = get_object_or_404(CustomUser, id=user_id)
+        user.delete()
+        return Response({"message": "User deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
